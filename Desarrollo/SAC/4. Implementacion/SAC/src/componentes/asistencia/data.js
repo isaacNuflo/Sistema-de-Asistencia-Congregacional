17b2d67e25c58f9b3dc670870d3ec9c43fe778d2
kevinMonteza <kmonteza95@gmail.com>
2018-11-20 13:41:40 -0500

    datos de alumnos

---
+++ Desarrollo/SAC/4. Implementacion/SAC/src/componentes/asistencia/data.js
@@ -0,0 +1,20 @@
+export default[
+	{
+        id:2,
+		nombre:'Mark',
+        apellidos:'asdadasd asdasdasd',
+        estado:0
+	},
+	{
+        id:4,
+		nombre:'Jacob',
+        apellidos:'kakakak asdasdasd',
+        estado:0
+	},
+	{
+        id:5,
+		nombre:'Larry',
+        apellidos:'asdasdasdasd adasdads',
+        estado:0
+	}
+]
